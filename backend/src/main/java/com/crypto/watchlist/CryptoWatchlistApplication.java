0121694b117656a9a0c66aa8f411ec27729e98ea
Novo-01 <novoweb.jp@gmail.com>
2025-04-26 18:58:03 +0900

    refactor: バックエンドのパッケージ構成を改善

---
+++ backend/src/main/java/com/crypto/watchlist/CryptoWatchlistApplication.java
@@ -0,0 +1,13 @@
+package com.crypto.watchlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoWatchlistApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CryptoWatchlistApplication.class, args);
+    }
+
+} 
